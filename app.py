9220ef20d6a9676e262e48a29b8ee4ccaff0234f
Chrollo <zamilemkohlakalii@gmail.com>
2023-06-01 18:52:58 +0200

    TRY

--- app.py
+++ app.py
@@ -1,22 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import openai
+
+openai.api_key = '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'
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('chat.html')
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    user_message = request.form['user_message']
-    # Process user message and generate chatbot reply
-    chatbot_reply = generate_chatbot_reply(user_message)
-    return {'chatbot_reply': chatbot_reply}
+@app.route('/message', methods=['POST'])
+def message():
+    user_message = request.form['message']
+    response = openai.ChatCompletion.create(
+        model="text-davinci-002",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role": "user",
+                "content": f"{user_message}"
+            }
+        ]
+    )
+    response_message = response['choices'][0]['message']['content']
+    return jsonify({'message': response_message})
 
-def generate_chatbot_reply(user_message):
-    # Implement your chatbot logic here to generate a reply based on user_message
-    # Return the chatbot reply
-    return "Chatbot reply goes here"
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
